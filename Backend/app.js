6c143d4f2c9bb72d8865782ee65a4d28138270c6
manish <manishkaswan88@gmail.com>
2023-11-08 23:26:05 +0530

    Frontend server address changed

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const mongoose = require("mongoose");
-// const cors = require('cors');
+const cors = require('cors');
+const HotelBooking = require("./models/hotelBooking");
 const app = express();
 
 const DATABASE = "mongodb+srv://manishkaswan88:gJLrwn18LEN6qooi@cluster0.z3dh8rj.mongodb.net/";
 const PORT = 3001;
-// const BASE_URL = "https://glittery-phoenix-690207.netlify.app";
+const BASE_URL = 'https://654bc8310c1a031ada264a67--luxury-tapioca-81cd63.netlify.app/';
 // const PORT = process.env.PORT;
 
-// app.use(cors({
-//   origin: `${BASE_URL}`,  
-//   credentials: true,  
-// }));
+app.use(cors({
+  origin: `${BASE_URL}`,  
+  credentials: true,  
+}));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
@@ -26,3 +27,21 @@ mongoose.connect(DATABASE)
 });
 
 app.use(express.json());
+
+app.get("/" , async(req , res) => {
+  try {
+      let hotelData = await HotelBooking.find();
+      console.log(hotelData);
+      if(hotelData) {
+          res.json ({
+              message: "hotelData Retrieved",
+              data: hotelData
+          });
+      }
+    }
+  catch(err) {
+      res.json ({
+          message: "Data Nahi Mila"
+      });
+  }
+});
